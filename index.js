f64616bf6e677341d6099cb3c8b25fdf3a34ac2c
erik hirsch <ehirsch760@gmail.com>
2020-05-12 21:41:43 -0700

    found template starting over...

--- index.js
+++ index.js
@@ -1,51 +1,12 @@
-var inquirer = require("inquirer");
-var fs = require("fs");
+const questions = [
 
-inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the title of the project?",
-      name: "project-name"
-    },
-    {
-      type: "input",
-      message: "Describe the project.",
-      name: "description"
-    },
-    {
-      type: "input",
-      message: "Installation?",
-      name: "install"
-    },
-    {
-        type: "input",
-        message: "Usage",
-        name: "use"
-      },
-      {
-        type: "input",
-        message: "What type of license will this project have?",
-        name: "license"
-      },
-      {
-        type: "input",
-        message: "Contributing.",
-        name: "contribution"
-      },
-      {
-        type: "input",
-        message: "Any questions?",
-        name: "questions"
-      },
-  ])
-  .then(function(response) {
-    fs.writeFile(response.name + "README.txt", JSON.stringify(response,null, '\t'), function(err) {
+];
 
-        if (err) {
-          return console.log(err);
-        }
-      
-        console.log("Success! "+ JSON.stringify(response));
-      console.log(response)
-      });
-  });
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
--- undefinedREADME.txt
+++
@@ -1,9 +0,0 @@
-{
-	"project-name": "Jibber Jabber",
-	"description": "Lots of Jibber and Jabber",
-	"install": "Sure why not!",
-	"use": "ALL THE TIME",
-	"license": "every one",
-	"contribution": "Me myself & I",
-	"questions": "of course..."
-}
